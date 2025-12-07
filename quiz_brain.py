8a34173933f8c49e1d3aa34605b45c8b55e71cea
ABHISHEK SHARMA <abhi95018sharma@gmail.com>
2025-12-07 21:41:01 +0530

    Implement QuizBrain class with question handling

---
+++ quiz_brain.py
@@ -0,0 +1,18 @@
+class QuizBrain:
+    def __init__(self,q_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = q_list
+
+    def next_question(self):
+        question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {question.text}(True/False)? ")
+        self.check_answer(user_answer,question.answer)
+
+    def check_answer(self,user_answer,correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print(f"Correct! {user_answer} is the correct answer.")
+        else:
+            print(f"Incorrect. {user_answer} is the incorrect answer.")
